a326592c4b5e2cd8ab200c6c1b8b3ed34861aedd
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-01 17:35:49 -0300

    req01 done

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
+permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)]
+
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        target_time_count = 0
+        for schedule in permanence_period:
+            target_time_count += 1 if target_time >= schedule[0] and target_time <= schedule[1] else 0
+        return target_time_count
+    except:
+        return None
+
+print(study_schedule(permanence_period, 'joao'))
