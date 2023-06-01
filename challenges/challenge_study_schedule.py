f3850146bd7674a9a9d3a12a8a60dff0948a1d27
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-01 18:37:02 -0300

    lint problems solved

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,12 +1,8 @@
-permanence_period = [(2, 2), (1, 2), (2, 3), (1, 5), (4, 5)]
-
-def study_schedule(permanence_period, target_time):
+def study_schedule(permanence_period, time):
     try:
-        target_time_count = 0
-        for schedule in permanence_period:
-            target_time_count += 1 if target_time >= schedule[0] and target_time <= schedule[1] else 0
-        return target_time_count
+        time_count = 0
+        for schdle in permanence_period:
+            time_count += 1 if time >= schdle[0] and time <= schdle[1] else 0
+        return time_count
     except:
         return None
-
-print(study_schedule(permanence_period, 'joao'))
