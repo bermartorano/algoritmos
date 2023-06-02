54a411b57e2084fa2f7b0ce2baedfa147932920c
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-02 19:40:32 -0300

    req03 done

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,15 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    try:
-        if word[low_index] == word[high_index]:
-            if high_index - low_index <= 1:
-                return True
-            next = word[low_index + 1: high_index]
-            return is_palindrome_recursive(next, 0, len(next) - 1)
+    if word == '':
         return False
-    except Exception:
-        return False
-
-
-teste = 'aaa'
-
-print(is_palindrome_recursive(teste, 0, len(teste) - 1))
+    if high_index < low_index:
+        return True
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+    return False
