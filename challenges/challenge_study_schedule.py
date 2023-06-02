8cf3ebf52163bc1d87a76c8970114c9bbeb741f4
Bernardo Martorano <bernardormartorano@gmail.com>
2023-06-02 14:22:12 -0300

    req 01 lint errors solved

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -4,5 +4,5 @@ def study_schedule(permanence_period, time):
         for schdle in permanence_period:
             time_count += 1 if time >= schdle[0] and time <= schdle[1] else 0
         return time_count
-    except:
+    except TypeError:
         return None
